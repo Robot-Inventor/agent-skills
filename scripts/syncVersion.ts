89727204986082a20edb1ea08a79d7576bd11079
roboin <50550561+Robot-Inventor@users.noreply.github.com>
2026-08-15 00:21:08 +0900

    fix: fix Codex plugin definition (#44)

--- scripts/syncVersion.ts
+++ scripts/syncVersion.ts
@@ -1,10 +1,10 @@
 import fs from "fs/promises";
 import packageJson from "../package.json";
-import pluginJson from "../plugin.json";
+import pluginJson from "../.codex-plugin/plugin.json";
 
 const main = async () => {
-  pluginJson.version = packageJson.version;
-  await fs.writeFile("plugin.json", JSON.stringify(pluginJson, null, 4));
+    pluginJson.version = packageJson.version;
+    await fs.writeFile(".codex-plugin/plugin.json", JSON.stringify(pluginJson, null, 4));
 };
 
 await main();
